9aedca38ad4e3d5c422387101683a17cda57a55d
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-05-31 12:32:09 +0700

    Initial Commit

---
+++ HotDog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDog
+//
+//  Created by Truong Nhat Anh on 31/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
